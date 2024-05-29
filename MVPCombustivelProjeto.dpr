00baaf5418c29017c9840ca08c7b41e39554c6c9
givanildofeitoza <givanildofeitoza@hotmail.com>
2024-05-29 17:56:22 -0300

    projeto inicializado

---
+++ MVPCombustivelProjeto.dpr
@@ -0,0 +1,14 @@
+program MVPCombustivelProjeto;
+
+uses
+  Vcl.Forms,
+  MVPCombustivel in 'MVPCombustivel.pas' {Form2};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm2, Form2);
+  Application.Run;
+end.
